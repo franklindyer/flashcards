7c5f0f7120e5b08d5c9b0533be3e41eea5f27270
Franklin Dyer <franklin+github@dyer.me>
2025-03-20 16:23:34 -0600

    improved emoji subber to support flag and zwj sequences

--- src/emojis.ts
+++ src/emojis.ts
@@ -1,15 +1,29 @@
-export function makeOpenmojiImage(emj: string) {
+export function makeOpenmojiImage(emjSeq: string) {
+    var emjs = emjSeq.split("\u200D");
+    var hex = emjs.map((emj) => emj.codePointAt(0)!.toString(16)!.toUpperCase());
+    var imgId = hex.join("-200D-");
+    return buildOpenmojiImage(imgId);
+}
+
+export function makeOpenmojiFlagImage(emjFlag: string) {
+    var imgId = [...emjFlag].map((emj) => emj.codePointAt(0)!.toString(16)!.toUpperCase()).join("-");
+    return buildOpenmojiImage(imgId); 
+}
+
+export function buildOpenmojiImage(emjId: string) {
     const img = document.createElement('img');
     img.classList.add("openmoji-svg-image");
-    var hex = emj.codePointAt(0)!.toString(16)!.toUpperCase();
-    var url = `https://cdn.jsdelivr.net/gh/hfg-gmuend/openmoji/color/svg/${hex}.svg`
+    var url = `https://cdn.jsdelivr.net/gh/hfg-gmuend/openmoji/color/svg/${emjId}.svg`;
     img.src = url;
     return img;
 }
 
 export function replaceEmojis(el: HTMLElement) {
     var txtInside = el.innerHTML;
-    txtInside = txtInside.replace(/\p{RGI_Emoji}/vg, function(match, capture) {
+    txtInside = txtInside.replace(/[🇦-🇿]{2}/ug, function(match, capture) {
+        return new XMLSerializer().serializeToString(makeOpenmojiFlagImage(match));
+    });
+    txtInside = txtInside.replace(/(\p{RGI_Emoji}\u200D)*\p{RGI_Emoji}/vg, function(match, capture) {
         return new XMLSerializer().serializeToString(makeOpenmojiImage(match));
     });
     el.innerHTML = txtInside;
