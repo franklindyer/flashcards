4fd1704eba0b38242698c09a5690b0a638b6fc44
franklindyer <franklin@dyer.me>
2025-07-07 17:07:20 -0600

    fixed infinite spinning bug with cloze SR decktype, allowed it to detect nonexistent words again

--- src/generic-preloader.ts
+++ src/generic-preloader.ts
@@ -7,29 +7,34 @@ export class Preloader<a> {
     valueCounts: IDictionary<number> = {};
     numPreload: number;
 
-    delaySeconds: number = 0.1;
+    delaySeconds: number = 1.0;
 
     constructor(numPreload: number) { 
         this.numPreload = numPreload;
     }
 
-    fillCacheForKey(k: string, fetcher: (k: string) => Promise<a[]>) {
+    fillCacheForKey(k: string, fetcher: (k: string) => Promise<a[]>): Promise<void> {
         var valuesNeeded = this.numPreload - this.valueCounts[k];
-        this.valueCounts[k] = this.numPreload;
-        var i = 0;
-        fetcher(k).then((xs) => xs.map((x) => this.values[k].push(x))).catch((e) => { console.log(e) });
+        return fetcher(k).then((xs) => {
+            if (xs === null || xs === undefined)
+                return;
+            xs.map((x) => this.values[k].push(x));
+            this.valueCounts[k] = this.values[k].length; 
+        }).catch((e) => { console.log(e); });
     }
 
-    addKey(k: string, fetcher: (k: string) => Promise<a[]>) {
+    addKey(k: string, fetcher: (k: string) => Promise<a[]>): Promise<void> {
         if (this.values[k] === undefined) {
             this.values[k] = [];
             this.valueCounts[k] = 0;
         }
-        this.fillCacheForKey(k, fetcher);
+        return this.fillCacheForKey(k, fetcher);
     }
 
-    getKey(k: string, fetcher: (k: string) => Promise<a[]>): Promise<a> {
-        this.addKey(k, fetcher);
+    getKey(k: string, fetcher: (k: string) => Promise<a[]>, maxAttempts: number = 3): Promise<a | undefined> {
+        if (maxAttempts == 0)
+            return new Promise((resolve, _) => resolve(undefined));
+        var keyAddedPromise = this.addKey(k, fetcher);
         if (this.values[k].length > 0) {
             return new Promise((resolve, _) => {
                 this.valueCounts[k] += -1;
@@ -38,7 +43,7 @@ export class Preloader<a> {
                 resolve(nextVal);
             });
         } else {
-            return new Promise((resolve, _) => setTimeout(() => resolve(this.getKey(k, fetcher)), 1000*this.delaySeconds));
+            return keyAddedPromise.then((_) => this.getKey(k, fetcher, maxAttempts-1));
         }
     }
 }
