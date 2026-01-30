8101222436c8c2a442cf098e51e91b6b40ded890
franklindyer <franklin@dyer.me>
2026-01-29 20:45:45 -0700

    added another unit test for SR newqueue

--- tests/sr-newqueue.test.ts
+++ tests/sr-newqueue.test.ts
@@ -24,7 +24,6 @@ describe('SR new queue works correctly', () => {
     test('precisely the cards marked as no longer new are removed', () => {
         var newOptions: string[] = numberStrings(100); 
         var q: SRNewQueue = emptySRQueue(10);
-        q = refillNewQueue(q, newOptions);
 
         var isStillNewSeq = [...[...new Array(100).keys()].map((n) => Math.random() < 0.5)]
         for (var b of isStillNewSeq) {
@@ -39,7 +38,24 @@ describe('SR new queue works correctly', () => {
         }
     });
 
-    // test('cards in new queue are studied in cyclic order', () => {});
+    test('cards in new queue are studied in cyclic order', () => {
+        var newOptions: string[] = numberStrings(100); 
+        var q: SRNewQueue = emptySRQueue(10);
+
+        var cardSeq: string[] = [];
+        for (var i = 0; i < 100; i++) {
+            var c = chooseNext(q, newOptions);
+            expect(c).toBeDefined();
+            cardSeq.push(c!);
+            q = incorporateLast(q, c, true);
+            expect(q.newQueue).toContain(c);
+        }
+
+        for (var i = 0; i < 100; i++) {
+            expect(cardSeq[i % 10]).toEqual(cardSeq[i]);
+        }       
+    });
+
     // test('queue is refilled in batches when the argument is set', () => {});
     // test('queue is refilled continuously when the argument is not set', () => {});
 
