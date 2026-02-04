36f9fcab84e97409e086483893fbf4812d2e167a
franklindyer <franklin@dyer.me>
2026-02-03 19:43:22 -0700

    added some planned tests for the SR deck

---
+++ tests/universal-sr-deck-tests.ts
@@ -0,0 +1,12 @@
+describe('SR deck runs without errors', () => {
+    // test('new and due cards can be studied', () => {});
+    // test('menu can be opened and modified without errors', () => {});
+    // test('all sorts of malformed SR decks are repaired correctly', () => {});
+});
+
+describe('SR deck logic is correct', () => {
+    // test('new cards transition to due after enough correct answers', () => {});
+    // test('a due card's interval gets longer iff answered correctly', () => {});
+    // test('practice cards do not affect the deck's state', () => {});
+    // test('cards become due at the time they are scheduled to', () => {});
+});
